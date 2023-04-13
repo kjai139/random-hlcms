1c4b56fe8f4d707cf17c5f69306e6eeaf5ead281
simon y <71997308+kjai139@users.noreply.github.com>
2023-04-13 14:59:54 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `randomHLCMS`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
