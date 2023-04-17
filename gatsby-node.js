d91abc96fbbedafc0a02107549a8cfeff7c44427
simon y <71997308+kjai139@users.noreply.github.com>
2023-04-17 19:58:05 -0400

    dsad

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,20 +2,7 @@ const path = require('path')
 const { paginate } = require("gatsby-awesome-pagination")
 
 
-// exports.createPages = async ({ actions, graphql }) => {
-//     const { createPage } = actions
 
-//     const archiveResult = await graphql(`
-//     query {
-//         allContentfulBlogPost(sort: {createdAt: DESC}) {
-           
-//             nodes {
-//                 slug
-//             }
-            
-//         }
-//     }
-//     `)
 
 
 exports.createPages = async function ({ actions, graphql }) {
