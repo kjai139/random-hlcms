9cb754875b8c41259a79ca7678992b757f1a83a0
simon y <71997308+kjai139@users.noreply.github.com>
2023-04-17 19:31:23 -0400

    og img fix

--- src/components/seo.js
+++ src/components/seo.js
@@ -28,7 +28,7 @@ const Seo = ({title, description, siteUrl, ogImg, ogTitle, ogDesc}) => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Noto+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
-        <meta property='og:image' content={ogImg ? ogImg : 'https://images.ctfassets.net/z6w0khw4r6q1/4gE0xQWPfopxSspwb62gBJ/d956f16d7cd8a5fb17c9312de2de8b86/logo.png' }></meta>
+        <meta property='og:image' content={ogImg ? ogImg : 'https://images.ctfassets.net/xjhjlusifrvm/53hFDWcYj3gFaUAG07YOq8/5c082616a9c52c26129b1ff9835f87f3/android-chrome-512x512.png' }></meta>
         <meta property='og:description' content={ogDesc? ogDesc : `Welcome to ${data.site.siteMetadata.title}, your ultimate destination for translated novels! Browse our extensive collection of translated novels, with new chapters regularly updated. Explore ${data.site.siteMetadata.title} now and indulge in the magic of translated novels`} />
         <meta property='og:title' content={title? `${title} | ${data.site.siteMetadata.title}` : `${data.site.siteMetadata.title}`}></meta>
         <meta property='og:image:secure_url' content={ogImg ? ogImg : '"https://images.ctfassets.net/xjhjlusifrvm/53hFDWcYj3gFaUAG07YOq8/5c082616a9c52c26129b1ff9835f87f3/android-chrome-512x512.png"'}></meta>
