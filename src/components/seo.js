44a533f886258b1ca146a8ac4070566167409d27
simon y <71997308+kjai139@users.noreply.github.com>
2023-05-15 15:10:30 -0400

    json ld script

--- src/components/seo.js
+++ src/components/seo.js
@@ -18,6 +18,12 @@ const Seo = ({title, description, siteUrl, ogImg, ogTitle, ogDesc}) => {
     `)
     
     // console.log(data.site.siteMetadata.title)
+    const ldJsonScript = {
+        "@context": "https://schema.org",
+        "type": "WebSite",
+        "name": data.site.siteMetadata.title,
+        "url": data.site.siteMetadata.siteUrl
+    };
 
 
     return (
@@ -30,6 +36,9 @@ const Seo = ({title, description, siteUrl, ogImg, ogTitle, ogDesc}) => {
         <meta property='og:description' content={ogDesc? ogDesc : `Welcome to ${data.site.siteMetadata.title}, your ultimate destination for translated novels! Browse our extensive collection of translated novels, with new chapters regularly updated. Explore ${data.site.siteMetadata.title} now and indulge in the magic of translated novels`} />
         <meta property='og:title' content={title? `${title} | ${data.site.siteMetadata.title}` : `${data.site.siteMetadata.title}`}></meta>
         <meta property='og:image:secure_url' content={ogImg ? ogImg : '"https://images.ctfassets.net/xjhjlusifrvm/53hFDWcYj3gFaUAG07YOq8/5c082616a9c52c26129b1ff9835f87f3/android-chrome-512x512.png"'}></meta>
+        <script type="application/ld+json">{JSON.stringify(ldJsonScript)}</script>
+
+        
         </>
 
     )
